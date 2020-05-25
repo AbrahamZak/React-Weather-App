5b1cd10092914678913f16a3f249d2e7ab9052ec
Abraham Zakharov <abrahamzakharov@gmail.com>
2020-05-25 14:55:07 -0400

    Updates

---
+++ precache-manifest.679ed5ae2d737d16bee619094a55896c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19eb73e0d11f9c7512ec93748f54d9cd",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "581a22f7609bf57669b1",
+    "url": "/React-Weather-App/static/css/main.31caa1b1.chunk.css"
+  },
+  {
+    "revision": "3edff441a3b08e953a25",
+    "url": "/React-Weather-App/static/js/2.65e6daee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Weather-App/static/js/2.65e6daee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "581a22f7609bf57669b1",
+    "url": "/React-Weather-App/static/js/main.83e8671f.chunk.js"
+  },
+  {
+    "revision": "43b3b53ff9189ff8b9d3",
+    "url": "/React-Weather-App/static/js/runtime-main.ac82a324.js"
+  }
+]);
